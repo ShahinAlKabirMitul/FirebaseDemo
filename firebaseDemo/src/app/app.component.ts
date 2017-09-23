63e3e51263c11ab35a3eee27d5e099ac5715b4d5
mitulbd <mitul.cse@gmail.com>
2017-09-23 18:09:32 +0600

    Unsubscribing from Subscriptions

--- firebaseDemo/src/app/app.component.ts
+++ firebaseDemo/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+
+import { any } from 'codelyzer/util/function';
+import { Component, OnDestroy } from '@angular/core';
+import{AngularFireDatabase} from 'angularfire2/database'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'app';
+  subscription:Subscription;
+  courses:any;
+ constructor(db:AngularFireDatabase){
+  this.subscription= db.list('/courses').subscribe(data=>{
+      this.courses=data;
+      console.log(this.courses);
+   });
+
+ }
+ ngOnDestroy(){
+   console.log('click');
+  this.subscription.unsubscribe();  
+ }
 }
